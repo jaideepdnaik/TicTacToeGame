172501b9dbc3a7a07ff4f3cef175499e8c4e027e
jaideepdnaik <jaideep.d@juegostudioz.com>
2025-03-10 11:44:50 +0530

    Add game loop and display logic in Program class.

--- TicTacToe.Core/Program.cs
+++ TicTacToe.Core/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using TicTacToe.Core.Models;
 
-Console.WriteLine("Hello, World!");
+namespace TicTacToe.Core;
+
+class Program
+{
+    static void Main()
+    {
+        var game = new TicTacToeGame();
+        
+        game.MakeMove(0);
+        game.MakeMove(3);
+        game.MakeMove(1);
+        game.MakeMove(4);
+        game.MakeMove(2);
+        
+        var board = game.GetBoard();
+        for (var i = 0; i < 9; i++)
+        {
+            Console.Write(board[i] + " ");
+            if ((i + 1) % 3 == 0) Console.WriteLine();
+        }
+
+        var winner = game.CheckWinner();
+        Console.WriteLine($"Winner: {(winner ?? "No winner yet")}");
+    }
+}
